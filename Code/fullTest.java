685ab3c8338bcf4fdc4bfbf403e8150a7cee4e9a
Andy <AndrewJasonKee@gmail.com>
2015-03-29 20:29:56 -0600

    the tester works!!!

--- Code/fullTest.java
+++ Code/fullTest.java
@@ -1,32 +1,27 @@
 import java.util.concurrent.locks.ReentrantLock;
 
-public class Test<T>{
+public class fullTest{
 
 	//Initialize class variables
-	private int NUMTHREADS 	= 8;
-	private int countToThis 	= 1000;
+	// private int NUMTHREADS 	= 8;
 	private static volatile int counter 	= 0;
-	private string lockType = "all";
 	Thread[] threads;
+	private CLH_Lock c_lock = new CLH_Lock();
+	private MCS_Lock m_lock = new MCS_Lock();
+	private ReentrantLock r_lock = new ReentrantLock();
+	// T lock;
 
 	//SubClass that is the "thread"
 	//multiple of these are started, and are stored 
 	//in the threads array
-	class threadInstance<T> extends Thread{
+	class threadInstance extends Thread{
 		private int threadID;
-		private T lock;
-		public threadInstance(int ID, string type){
-			this.threadID = ID;
-			switch (type) {
-				case "CLH":
-					lock = new CLH_Lock();
-				break;
-				case "MCS":
-
-				break;
-				case "java":
-				break;
-			}
+		private String type;
+		private int countToThis;
+		public threadInstance(int ID, String type, int countToThis){
+			this.threadID 		= ID;
+			this.type 			= type;
+			this.countToThis 	= countToThis;
 		}
 
 
@@ -34,13 +29,33 @@ public class Test<T>{
 		//when initiated.  Uses the tree lock to prevent
 		//other threads from editting.
 		public void run(){	
-			for ( int i = 0; i < countToThis; i++ ){
-				lock.lock();
+			for ( int i = 0; i < this.countToThis; i++ ){
+				switch(this.type){
+					case "CLH":
+						c_lock.lock();
+					break;	
+					case "MCS":
+						m_lock.lock();
+					break;
+					case "java":
+						r_lock.lock();
+					break;
+				}
 				try{
 					counter = counter + 1;
 				}
 				finally{
-					lock.unlock();
+					switch(this.type){
+						case "CLH":
+							c_lock.unlock();
+						break;	
+						case "MCS":
+							m_lock.unlock();
+						break;
+						case "java":
+							r_lock.unlock();
+						break;
+					}
 				}
 			}
 		}
@@ -48,10 +63,10 @@ public class Test<T>{
 
 	//function to run the test.  creates the threads, 
 	//starts them, and joins them
-	private void test(string type){
+	private void test(String type, int NUMTHREADS, int countToThis){
 		threads = new Thread[NUMTHREADS];
 		for (int i = 0; i < NUMTHREADS; i++){
-			threads[i] = new threadInstance( i , type );
+			threads[i] = new threadInstance( i , type, countToThis);
 			threads[i].start();
 		}
 		for (int i = 0; i < NUMTHREADS; i++){
@@ -65,40 +80,43 @@ public class Test<T>{
 	//Tests the lock many times.  outputs the result at the end
 	public static void main(String args[]){
 		int numTries = Integer.parseInt(args[0]);
-		NUMTHREADS = Integer.parseInt(args[1]);
-		countToThis = Integer.parseInt(args[2]);
-		lockType = args[3];
+		int NUMTHREADS = Integer.parseInt(args[1]);
+		int countToThis = Integer.parseInt(args[2]);
+		String lockType = args[3];
 
-		if (lockType == "CLH" || lockType == "all") {
+		System.out.println(lockType);
+		if (lockType.equals("CLH") || lockType.equals("all")) {
 			long startTime = System.nanoTime();
 		 	for(int i = 0; i < numTries; i++){
-				Test Tester = new Test();
+				fullTest Tester = new fullTest();
 				try{
-					Tester.test("CLH");
+					Tester.test("CLH", NUMTHREADS, countToThis);
 				}
 				catch(Exception e) {}
 				counter = 0;
 			}
 			long endTime = System.nanoTime();
 			System.out.println((endTime - startTime)/1000000 + " milliseconds");
-		} if (lockType == "MCS" || lockType == "all") {
+		} 
+		if (lockType.equals("MCS") || lockType.equals("all")) {
 			long startTime = System.nanoTime();
 		 	for(int i = 0; i < numTries; i++){
-				Test Tester = new Test();
+				fullTest Tester = new fullTest();
 				try{
-					Tester.test("MCS");
+					Tester.test("MCS", NUMTHREADS, countToThis);
 				}
 				catch(Exception e) {}
 				counter = 0;
 			}
 			long endTime = System.nanoTime();
 			System.out.println((endTime - startTime)/1000000 + " milliseconds");
-		} if (lockType == "java" || lockType == "all") {
+		} 
+		if (lockType.equals("java") || lockType.equals("all")) {
 			long startTime = System.nanoTime();
 		 	for(int i = 0; i < numTries; i++){
-				Test Tester = new Test();
+				fullTest Tester = new fullTest();
 				try{
-					Tester.test("java");
+					Tester.test("java", NUMTHREADS, countToThis);
 				}
 				catch(Exception e) {}
 				counter = 0;
